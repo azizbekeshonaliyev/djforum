7d55b2564c9ad862c86a9cf45ff7a700d093e11b
azizbekeshonaliyev <1996azizbekeshonaliyev@gmail.com>
2020-08-06 09:54:23 +0500

    djangorestframework

--- forum/settings.py
+++ forum/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'question.apps.QuestionConfig',
     'tinymce',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- forum/urls.py
+++ forum/urls.py
@@ -15,9 +15,40 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
+from question.models import Tag
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['name', 'status','pub_date']
+
+# ViewSets define the view behavior.
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'tags', TagViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('questions/', include('question.urls')),
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
+    path(r'api-auth/', include('rest_framework.urls'))
 ]
